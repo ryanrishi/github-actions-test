ed1d0f374a692ac3384cdbd7291f4851eb69860b
Ryan Rishi <ryan@ryanrishi.com>
2019-08-27 10:49:45 -0700

    initial commit

---
+++ src/test/java/com/ryanrishi/githubactionstest/GithubActionsTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ryanrishi.githubactionstest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubActionsTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
